f58d5361012abd8293c7092374551e347d3cc388
Pavelc4 <pavelc4@users.noreply.github.com>
2026-05-27 04:07:51 +0700

    chore: init hypr and scripts directory structure
    Change-Id: I00000000cbc9c6825e3e436f689b271da3292c68

---
+++ hypr/hyprland.lua
@@ -0,0 +1,15 @@
+-- Yoru Hyprland Lua Entry Point
+-- Sourced in ~/.config/hypr/hyprland.lua
+
+-- Internal helpers
+require("hyprland.lib")
+
+-- Environment variables
+require("hyprland.env")
+
+-- Configurations
+require("hyprland.variables")
+require("hyprland.execs")
+require("hyprland.general")
+require("hyprland.rules")
+require("hyprland.keybinds")
